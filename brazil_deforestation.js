bc7b3f0214da8f5cf670c22c16cb1d675e42830b
Celso H. L. Silva Junior <celsohlsj@gmail.com>
2025-08-15 09:56:04 -0300

    Update and rename amazonia_deforestation_v2.js to brazil_deforestation.js

--- brazil_deforestation.js
+++ brazil_deforestation.js
@@ -1,11 +1,12 @@
 // Auxiliary Dataset
 var brazil = ee.FeatureCollection("users/celsohlsj/brazil");
 
-//MapBiomas Brazil Collection 9
+//MapBiomas Brazil Collection 10
 var mapbiomas = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection9/mapbiomas_collection90_integration_v1'); 
 
 // Forest Cover in 1985
-var forest_1985 = mapbiomas.select('classification_1985').remap([3,4,5,6,49,11,12,32,50], [1,1,1,1,1,1,1,1,1], 0);
+var forest_1985 = mapbiomas.select('classification_1985').remap([3,6], [1,1], 0); // Only Forest Formation
+//var forest_1985 = mapbiomas.select('classification_1985').remap([3,6,49,11,12,32,50], [1,1,1,1,1,1,1,1,1], 0);
 
 var empty = ee.Image().byte();
 for (var i=0; i<38; i++)  { // 1986 to 2023
@@ -13,8 +14,8 @@ for (var i=0; i<38; i++)  { // 1986 to 2023
     // The anthropic mask is generated from the year 1986
     var y = 1986+i;
     var year = 'classification_'+y;
-    var oldvalues = ee.List([9,15,19,20,21,35,39,40,41,46,47,48,62]);
-    var newvalues = ee.List([2,2,2,2,2,2,2,2,2,2,2,2,2]);
+    var oldvalues = ee.List([15, 19, 39, 20, 40, 62, 41, 46, 47, 35, 48, 9, 21, 24, 30]);
+    var newvalues = ee.List([2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]);
     var anthropic = mapbiomas.select(year).remap(oldvalues, newvalues, 0);
     
 	// Here we combine forest map (year_i) with athropic mask map (year_i+1) to mapping deforestation
@@ -29,7 +30,7 @@ var mapbiomas_forest_loss = empty.select(empty.bandNames().slice(1));
 
 // Bringing All Deforestation Bands Together Into One
 var empty = ee.Image().byte();
-for (var i=1986; i<2024; i++)  { // 1986 to 2023
+for (var i=1986; i<2025; i++)  { // 1986 to 2024
 	var year = mapbiomas_forest_loss.select("classification_"+i);
 	var year = year.multiply(i).rename(ee.String("year_"+i));
 	empty = empty.addBands(year);
@@ -40,15 +41,16 @@ var bandNames = image.bandNames();
 
 var toCollection = ee.ImageCollection.fromImages(bandNames.map(function(name){
   name = ee.String(name);
-  // select one band an put into an image. You might need a specific cast and renaming of the bands
+  // select one band and put it into an image. You might need a specific cast and renaming of the bands
   return image.select(name).rename('newName').toFloat();
 }));
 var summedBand = toCollection.sum().toInt16();
 
 Export.image.toAsset({
       image: forest_1985.blend(summedBand.updateMask(summedBand.neq(0))),
-      description: 'public/brazil_deforestation_vegetation_1986_2023_collection9_v2',
+      description: 'public/brazil_deforestation_vegetation_1986_2024_collection9_v3',
       scale: 30, 
       region: brazil.geometry().bounds(),
       maxPixels:1e13
 });
+
